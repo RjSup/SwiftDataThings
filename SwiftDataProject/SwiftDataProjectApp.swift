09fca9164c93f386758a59244e79a608c6745ee0
RjSup <19141558@brookes.ac.uk>
2025-09-30 16:09:25 +0100

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ryan on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
