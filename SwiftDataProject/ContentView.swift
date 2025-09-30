b2d620836503fde5ee3c7eba6f8ce97b30ca3e08
RjSup <19141558@brookes.ac.uk>
2025-09-30 17:01:18 +0100

    Learning about SwiftData

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    // get the swiftdata context
+    @Environment(\.modelContext) var modelContext
+    // query swiftdata for user User.name instances
+    // using a predicate to access the exact data we need
+    @Query(filter: #Predicate<User> { user in
+        user.name.localizedStandardContains("R") &&
+        user.city == "New York" ||
+        user.name.localizedStandardContains("J") &&
+        user.city == "London"
+    }, sort: \User.name) var users: [User]
+    
+    // store a path for navigation
+    @State private var path: [User] = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // navigation context using the path
+        NavigationStack {
+            List(users) { user in
+                Text(user.name)
+            }
+            .navigationTitle("Users")
+            .toolbar {
+                Button("Add Samples", systemImage: "plus") {
+                    // ensure database is clear before add to it
+                    try? modelContext.delete(model: User.self)
+                    
+                    // add data to db
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ SwiftDataProject/EditUserVIew.swift
@@ -0,0 +1,35 @@
+//
+//  EditUserVIew.swift
+//  SwiftDataProject
+//
+//  Created by Ryan on 30/09/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct EditUserVIew: View {
+    @Bindable var user: User
+    
+    var body: some View {
+        Form {
+                 TextField("Name", text: $user.name)
+                 TextField("City", text: $user.city)
+                 DatePicker("Join Date", selection: $user.joinDate)
+             }
+             .navigationTitle("Edit User")
+             .navigationBarTitleDisplayMode(.inline)
+         }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: User.self, configurations: config)
+        let user = User(name: "Ryan", city: "Oxford", joinDate: .now)
+        return EditUserVIew(user: user)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create container: \(error.localizedDescription)")
+    }
+}
